87a8bafe3a262033d4915fe9c7cb9f5e89a2fac8
Reinaesaya <xseanhylx@gmail.com>
2015-01-21 16:44:24 -0500

    Navigation updated to compensate for false range detection. Turning not working as of yet

--- Navigation/Navigation.ino
+++ Navigation/Navigation.ino
@@ -5,29 +5,71 @@ Ultrasonic ultrasonic(8,9);
 int left_motor = 5;
 int right_motor = 4;
 
+long range_array[5] = {100,100,100,100,100};
+
 void setup() {
   Serial.begin(9600);
   pinMode(left_motor, OUTPUT);
   pinMode(right_motor, OUTPUT);
-  digitalWrite(left_motor, HIGH);
-  digitalWrite(right_motor, HIGH);
 }
 
 void loop()
 {
   long obj_range = ultrasonic.Ranging(CM);
+  
+  Serial.print("Object Range: \t");
   Serial.println(obj_range);
-  if (obj_range < 10) {
-    digitalWrite(left_motor, LOW);
-    digitalWrite(right_motor, LOW);
+  
+  shift_add(range_array, 5, obj_range);
+  if (check_array(range_array, 5, 30) == 1) {
+    analogWrite(left_motor, 0);
+    analogWrite(right_motor, 0);
+    
+    delay(100);
+    turn('l', 2000);
   }
   else {
-    digitalWrite(left_motor, HIGH);
-    digitalWrite(right_motor, HIGH);
+    analogWrite(right_motor, 64);
+    analogWrite(left_motor, 64);
   }
-  delay(100);
+  delay(50);
 }
 
+void turn(char direction, int time) {
+  Serial.println("TURN");
+  analogWrite(left_motor, 0);
+  analogWrite(right_motor, 0);
+  
+  if (direction == 'l') {
+    Serial.println("LEFT");
+    analogWrite(right_motor, 64);
+  }
+  else {
+    Serial.println("RIGHT");
+    analogWrite(left_motor, 64);
+  }
+  Serial.println("Gothere");
+  delay(time);
+}
 
+void shift_add(long* arr, int length, long b) {
+  for (int i = 0; i != length; i++) {
+    *(arr + i) = *(arr + i + 1);
+  }
+  arr[4] = b;
+}
 
-
+int check_array(long* arr, int length, long thresh) {
+  
+  int ret = 1;
+  
+  for (int i = 0; i != length; i++) {
+    Serial.print(arr[i]);
+    Serial.print("\t");
+    if (arr[i] >= thresh) {
+      ret = 0;
+    }
+  }
+  Serial.print("\n");
+  return ret;
+}
