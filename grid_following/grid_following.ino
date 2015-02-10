1054d3fa098694890f751b759d8fdabd32673801
Reinaesaya <xseanhylx@gmail.com>
2015-02-10 16:38:25 -0500

    Debugged grid-following (PWM can't be too low) and changed format

--- grid_following/grid_following.ino
+++ grid_following/grid_following.ino
@@ -8,84 +8,111 @@ const int MaxWhite = 990;
 
 DiffSteering MyWheel(13,12,11,10,9);
 
-void setup(){
-  pinMode(Rsensor, INPUT);
-  pinMode(Lsensor, INPUT);
-  pinMode(Bsensor, INPUT);
-  pinMode(Fsensor, INPUT); 
+void setup()
+{
   Serial.begin(9600);
 }
 
-void loop(){
+void loop()
+{
   byte num_inter = 0;
   byte num_turn = 0;
-  int Fsensor = analogRead(A12);
-  int Rsensor = analogRead(A15);
-  int Lsensor = analogRead(A13);
-  int Bsensor = analogRead(A14);
+  int Fsensor = analogRead(A15);
+  int Rsensor = analogRead(A13);
+  int Lsensor = analogRead(A14);
+  int Bsensor = analogRead(A12);
+  Serial.print(Fsensor);
+  Serial.print(' ');
   Serial.print(Rsensor);
   Serial.print(' ');
   Serial.print(Lsensor);
   Serial.print(' ');
-  Serial.print(Bsensor);
-  Serial.print(' ');
-  Serial.println(Fsensor); 
+  Serial.println(Bsensor); 
   // Collaboration 
-  long collab_arrayR[10] = {0};
-  long collab_arrayL[10] = {0};
-  long collab_arrayB[10] = {0};
-  long collab_arrayF[10] = {0};
+  long collab_arrayR[10] = {0,0,0,0,0,0,0,0,0,0};
+  long collab_arrayL[10] = {0,0,0,0,0,0,0,0,0,0};
+  long collab_arrayB[10] = {0,0,0,0,0,0,0,0,0,0};
+  long collab_arrayF[10] = {0,0,0,0,0,0,0,0,0,0};
   int MaxWhiteR = 0;
   int MaxWhiteL = 0;
   int MinBlackB = 0;
   int MinBlackF = 0;
         
-  while (millis() <2000){
-    shift_add(collab_arrayR, 10, Rsensor);
-    shift_add(collab_arrayL, 10, Lsensor);
-    shift_add(collab_arrayB, 10, Bsensor);
-    shift_add(collab_arrayF, 10, Fsensor);
-    MaxWhiteR = average(collab_arrayR, 10);
-    MaxWhiteL = average(collab_arrayL, 10);
-    MinBlackF = average(collab_arrayF, 10);
-    MinBlackB = average(collab_arrayB, 10);
+//  while (millis() <2000)
+//  {
+//    shift_add(collab_arrayR, 10, Rsensor);
+//    shift_add(collab_arrayL, 10, Lsensor);
+//    shift_add(collab_arrayB, 10, Bsensor);
+//    shift_add(collab_arrayF, 10, Fsensor);
+//    MaxWhiteR = average(collab_arrayR, 10);
+//    MaxWhiteL = average(collab_arrayL, 10);
+//    MinBlackF = average(collab_arrayF, 10);
+//    MinBlackB = average(collab_arrayB, 10);
+//  }
     
-  if (Fsensor < MaxWhite){        //front sensor detects white
+  if (Fsensor < MaxWhite)
+  { // If front sensor detects white
     //if (Bsensor > MaxWhite){      //back sensor detects black
-      if(Lsensor > MaxWhite && Rsensor < MaxWhite){
-         MyWheel.Pivot_L(90);}
-       else if(Rsensor > MaxWhite && Lsensor < MaxWhite){
-         MyWheel.Pivot_R(90);}
-       else if (Rsensor < MaxWhite && Lsensor<MaxWhite){        //pause for now...
-         MyWheel.Forward(25);
-         Serial.println("third");}
-       else{
-         MyWheel.Forward(0);
-         Serial.println("fourth");}
-       delay(500);
+    if(Lsensor > MaxWhite && Rsensor < MaxWhite)
+    { // If left detects black and right detects white
+      // Car front shifted right, this turn left
+      MyWheel.Pivot_L(90);
+      Serial.println("1");
+    }
+    else if(Rsensor > MaxWhite && Lsensor < MaxWhite)
+    {
+      MyWheel.Pivot_R(90);
+      Serial.println("2");
+    }
+    else if (Rsensor < MaxWhite && Lsensor<MaxWhite)
+    {        //pause for now...
+      MyWheel.Forward(90);
+      Serial.println("3");
+    }
+    else
+    {
+      MyWheel.Stop();
+      Serial.println("4");
+    }
+    delay(500);
+  }
     //}
     //else{  //back sensor detects white 
      // MyWheel.Forward(0);}  //pause for now...
-  }
-  else if(Fsensor > MaxWhite){  //front sensor detects black
-     if (Rsensor > MaxWhite && Lsensor > MaxWhite && Bsensor > MaxWhite){  // @the intersection
-       num_inter+= 1;
-       MyWheel.Forward(0);
+  else if(Fsensor > MaxWhite)
+  {  //front sensor detects black
+    if (Rsensor > MaxWhite && Lsensor > MaxWhite && Bsensor > MaxWhite)
+    {  // @the intersection
+      num_inter+= 1;
        //DETERMIN_DIREC(XXX);
-       byte DIRECT = 0;
-       if(DIRECT == 1){
-         MyWheel.Pivot_R(90);
-         num_turn += 1;
-         delay(900);}
-       else if (DIRECT == 2){
-         MyWheel.Pivot_L(90);
-         num_turn += 1;
-         delay(900);}
-       else if (DIRECT == 0){
-         MyWheel.Forward(25);}
-       }
-     else if (Rsensor < MaxWhite && Lsensor < MaxWhite && Bsensor > MaxWhite){    //line following...
-       MyWheel.Forward(25);}
+      Serial.println("5");
+      byte DIRECT = 0;
+      if(DIRECT == 1)
+      {
+        MyWheel.Pivot_R(90);
+        Serial.println("6");
+        num_turn += 1;
+        delay(900);
+      }
+      else if (DIRECT == 2)
+      {
+        MyWheel.Pivot_L(90);
+        Serial.println("7");
+        num_turn += 1;
+        delay(900);
+      }
+      else if (DIRECT == 0)
+      {
+        MyWheel.Forward(90);
+        Serial.println("8");
+        delay(1000);
+      }
+    }
+    else if (Rsensor < MaxWhite && Lsensor < MaxWhite && Bsensor > MaxWhite)
+    {    //line following...
+      MyWheel.Forward(90);
+      Serial.println("9");
+    }
   }
 }
   
